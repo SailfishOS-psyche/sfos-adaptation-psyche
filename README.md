9eb6177ecfb3f3c332f45f2ecb34a3dac0de7419
Verevka <verevka86@gmail.com>
2024-08-15 14:41:22 +0300

    init adaptation psyche v1.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# sfos-adaptation-vayu
+# sfos-adaptation-psyche
 
-With this adaptation, sailfish os is no longer tied to the android version installed on the phone, the modified LineageOS 18.1 is used as a basis.
+With this adaptation, sailfish os is no longer tied to the android version installed on the phone, the modified LineageOS 20.0 is used as a basis.
 After installing sailfishos.zip and adaptation.zip, in your sdcard will be created by droid-boot.img, droid-dtbo.img, hybrid-boot.img and hybris-dtbo.img. By flashing them through the recovery to the appropriate partition, you will be able to switch between sailfishos/android.
 
 For SailfishOS to work, your /data partition does not need to be encrypted!!!
--- setup.sh
+++ setup.sh
@@ -1,29 +1,12 @@
-# Sfos Adaptation for the Xiaomi Poco X3 Pro(vayu)
+# Sfos Adaptation for the Xiaomi 12X(psyche)
 
 FIRMWARE="/data/.stowaways/firmware";
-SFOS="/data/.stowaways/sailfishos";
-PATCHES="/data/.stowaways/patches";
 OUTFD=/proc/self/fd/$1;
 VENDOR_DEVICE_PROP=`grep ro.product.vendor.device /vendor/build.prop | cut -d "=" -f 2 | awk '{print tolower($0)}'`;
 
 # ui_print <text>
 ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
 
-# Copy files
-ui_print "Copying device adaptation files...";
-cp -r sfos/* $SFOS/;
-
 ui_print "Copying firmware images";
 mkdir $FIRMWARE;
 cp -r firmware/* $FIRMWARE/;
-
-ui_print "Copying patches staff";
-mkdir $PATCHES;
-cp -r patches/* $PATCHES/;
-
-# flash dtbo.img
-ui_print "Flash and backup dtbo images";
-dd if=/dev/block/bootdevice/by-name/dtbo of=$SFOS/boot/droid-dtbo.img;
-ln -s $SFOS/boot/droid-dtbo.img /sdcard;
-ln -s $FIRMWARE/hybris-dtbo.img /sdcard;
-flash_image /dev/block/bootdevice/by-name/dtbo firmware/hybris-dtbo.img;
